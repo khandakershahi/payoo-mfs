a4c6ef7eab39ebb6319035e288e32ee17c4b347a
khandakershahi <khandakershahi@gmail.com>
2025-08-22 23:12:31 +0600

    login page complete and home page add

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@
 
 document.getElementById('loginButton').addEventListener('click', function(e){
     e.preventDefault();
-    console.log('click');
-    console.log(e);
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById('pin-number').value;
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    // console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href = "./home.html";
+    }else{
+       alert('Invalid credentials');
+    }
+
 })
