a234827de52bffc1df56ca9013f48e70a2b0b6c5
khandakershahi <khandakershahi@gmail.com>
2025-08-22 22:54:26 +0600

    login button function add

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// login button function
+
+document.getElementById('loginButton').addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('click');
+    console.log(e);
+})
