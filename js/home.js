019ac2ac484a29156e657cb24e4d0aed813b7702
khandakershahi <khandakershahi@gmail.com>
2025-08-23 19:55:14 +0600

    cashout button function add

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 const validPin = 1234;
-
+// add-money feature
 document.getElementById('add-money-btn').addEventListener('click', function(e){
 e.preventDefault();
 
@@ -26,3 +26,33 @@ const TotalNewAvialableBalance = amount + availableBalance;
 
 document.getElementById('available-balance').innerText = TotalNewAvialableBalance;
 })
+
+
+// cashout money feature
+
+document.getElementById('withdraw-btn').addEventListener('click', function(e){
+    e.preventDefault();
+   const amount = parseInt(document.getElementById('withdraw-amount').value);
+   const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+
+   TotalNewAvialableBalance = availableBalance - amount;
+  
+    document.getElementById('available-balance').innerText = TotalNewAvialableBalance;
+})
+
+
+// toggling feature 
+document.getElementById('add-money-button').addEventListener(
+    'click', function(){
+        document.getElementById('cash-out-parent').style.display = 'none';
+        document.getElementById('add-money-parent').style.display = 'block';
+
+    }
+)
+document.getElementById('cash-out-button').addEventListener(
+    'click', function(){
+        document.getElementById('add-money-parent').style.display = 'none';
+        document.getElementById('cash-out-parent').style.display = 'block';
+
+    }
+)
